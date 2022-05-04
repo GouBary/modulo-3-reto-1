234a40eda77e83b7a1206d92e5dd889bba0b42e5
GouBary <juanblaze7342@gmail.com>
2022-05-04 18:20:23 -0500

    ejercisio1

---
+++ js/app.js
@@ -0,0 +1,23 @@
+let total = false ;
+
+const nivelOS = (procentaje) =>{
+    let osadia = prompt("¿Cual es el porcentaje de osadia del candidato?");
+    if (osadia>= 40) {
+        alert("Jeanine, puedes proceder con la dominación total!")
+    }
+else{
+    alert("Janine, ¡te tocó aplazar tu maléfico plan!")
+}
+}
+
+
+nivelOS(1);
+nivelOS(2);
+nivelOS(3);
+nivelOS(4);
+nivelOS(5);
+nivelOS(6);
+nivelOS(7);
+nivelOS(8);
+nivelOS(9);
+nivelOS(10);
